ee72d9b5d92f2c3a88488712e5cf7d268305ab57
Shibinjohn02 <shibinjohn19@gmail.com>
2021-11-20 07:07:04 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-gq26vg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-gq26vg)
---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component, VERSION, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AppComponent {
+  name = 'Angular ' + VERSION.major;
+
+  userObject: any = {};
+  constructor() {}
+
+  ngOnInit() {
+    this.userObject = {
+      name: 'Shibin John',
+      experience: 20,
+    };
+  }
+
+  increaseExperience() {
+    this.userObject.experience++;
+    // Immutability
+    // this.userObject = {
+    //   name: 'Angular coder',
+    //   experience: 40,
+    // };
+  }
+
+  decreaseExperience() {
+    this.userObject.experience--;
+  }
+}
